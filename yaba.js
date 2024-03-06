d3f008dec2e640c10121e8d77b4a0377e2642f96
rashidyoussef <rashid_gaber@hotmail.com>
2024-03-06 14:57:30 -0600

    ya tiene partida final

--- yaba.js
+++ yaba.js
@@ -4,6 +4,18 @@ let scissors = 2;
 let choice; 
 let comput;
 let resultado;
+let playerscore = 0;
+let compuscore = 0;
+
+function game() {
+    rockpaperscissor();
+    rockpaperscissor();
+    rockpaperscissor();
+    rockpaperscissor();
+    rockpaperscissor();
+}
+
+game();
 
 function rockpaperscissor() {
     function playerchoice() {
@@ -31,39 +43,64 @@ function rockpaperscissor() {
     if (choice == 'PIEDRA' || choice == 'ROCK') {
         if (comput == 0) {
                 resultado = 'TIE';
+                return resultado;
             }
             else if (comput == 1) {
                 resultado = 'LOSE';
+                return resultado;
                 }
             else {
                 resultado = 'WIN';
-        }
+                return resultado;
+                        }                
     }
 
     if (choice == 'PAPEL' || choice == 'PAPER') {
         if (comput == 0) {
             resultado = 'WIN';
+            return resultado;
         }
         else if (comput == 1) {
             resultado = 'TIE';
+            return resultado;
             }
         else {
             resultado = 'LOSE';
+            return resultado;
         }
     }
 
     if (choice == 'TIJERA' || choice == 'SCISSORS' || choice == 'TIJERAS') {
         if (comput == 0) {
             resultado = 'LOSE';
+            return resultado;
         }
         else if (comput == 1) {
             resultado = 'WIN';
+            return resultado;
             }
         else {
             resultado = 'TIE';
+            return resultado;
         }
     }
     }
+    if (resultado = 'WIN') {
+        playerscore++;
+        return playerscore;
+    }
+    else if (resultado = 'LOSE'){
+        compuscore++;
+        return compuscore;
+    }
+    else if (resultado = 'TIE'){
+        playerscore++;
+        compuscore++;
+        return playerscore;
+        return compuscore;
+    }
+  
+
 
     calcular();
 
@@ -78,4 +115,16 @@ function rockpaperscissor() {
     }
     }
 
-rockpaperscissor();
+    if (playerscore > compuscore) {
+        console.log('Ganaste la partida final');
+    }
+    else if (playerscore = compuscore) {
+        console.log('Empataste con una computadora jajajaja');
+    }
+    else if (playerscore < compuscore) {
+        console.log('Nmms perdiste jsjsjsjs');
+    }
+
+    
+    console.log(compuscore);
+    console.log(playerscore);
